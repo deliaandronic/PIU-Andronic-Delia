9b3d0690929185b37fc905ec218ac5999d6f9139
deliaandronic <156707150+deliaandronic@users.noreply.github.com>
2024-04-23 22:04:14 +0300

    Add files via upload

---
+++ Tema2-PIU/ClassAuto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProiectPIU
+{
+    internal class Auto
+    {
+
+        //constante
+        private const char SEPARATOR_FISIER = ',';
+        private const int ID = 0;
+        private const int MARCA = 1;
+        private const int CULOARE = 2;
+        private const int AN = 3;
+        private const int VIN = 4;
+        private const int INCHIRIAT = 5;
+
+        //proprietati
+        private int IdAuto { set; get; }
+        private string marca { set; get; }
+        private string culoare { set; get; }
+        private string an { set; get; }
+        private string vin { set; get; }
+        private string inchiriat { set; get; }
+
+    }
+}
